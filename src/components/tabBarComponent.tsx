90b9fb5ec3fb25c785faff989376dac10e934f85
Tayfun Akgün <tayfun.akgun@sestek.com>
2022-10-14 13:09:00 +0300

    Tabbar comp eklendi.

--- src/components/tabBarComponent.tsx
+++ src/components/tabBarComponent.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { Image, TouchableOpacity, View } from "react-native";
+import { Dimensions, Image, TouchableOpacity, View } from "react-native";
 import { DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
-import { KnovvuMainLogo2 } from "@src/assests";
-import { Text } from "@rneui/base";
+import { Knovvu32, KnovvuMainLogo2 } from "@src/assests";
+import { Button, Text } from "@rneui/base";
 import { useAppSelector } from "@src/utils/redux/hooks";
+import BusinessComponent from "@src/utils/functions/businessComponent";
+import { CtIconEnum } from "@src/utils/types";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 function TabBarComponent({ state, descriptors, navigation }) {
+
+    const color_100 = useAppSelector(state => state.theme.color_100);
+    const color_200 = useAppSelector(state => state.theme.color_200);
+    const color_300 = useAppSelector(state => state.theme.color_300);
+
+    const modalVisible = useAppSelector(state => state.main.modalVisible);
+
+    const dynamicWidth = Dimensions.get('screen').width * (1 / state.routes?.length);
+
     return (
-        <View style={{ flexDirection: 'row' }}>
-            {state.routes.map((route: any, index: number) => {
+        <View style={{ flexDirection: 'row', justifyContent: 'center', borderTopWidth: 0.2, borderTopColor: color_300 }}>
+            {state.routes.map((route, index) => {
                 const { options } = descriptors[route.key];
                 const label =
                     options.tabBarLabel !== undefined
@@ -20,6 +32,12 @@ function TabBarComponent({ state, descriptors, navigation }) {
                 const isFocused = state.index === index;
 
                 const onPress = () => {
+
+                    if (route.name === "Knovvu" && modalVisible) {
+                        modalVisible();
+                        return;
+                    }
+
                     const event = navigation.emit({
                         type: 'tabPress',
                         target: route.key,
@@ -27,7 +45,6 @@ function TabBarComponent({ state, descriptors, navigation }) {
                     });
 
                     if (!isFocused && !event.defaultPrevented) {
-                        // The `merge: true` option makes sure that the params inside the tab screen are preserved
                         navigation.navigate({ name: route.name, merge: true });
                     }
                 };
@@ -39,6 +56,34 @@ function TabBarComponent({ state, descriptors, navigation }) {
                     });
                 };
 
+                const getIconFunc = (focused: boolean) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        iconName = focused
+                            ? 'ios-home'
+                            : 'ios-home-outline';
+                    } else if (route.name === 'Settings') {
+                        iconName = focused ? 'ios-build' : 'ios-build-outline';
+                    }
+                    else if (route.name === 'About') {
+                        iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+                    }
+                    else if (route.name === 'Contact Us') {
+                        iconName = focused ? 'ios-mail-sharp' : 'ios-mail-outline';
+                    }
+                    else if (route.name === 'Knovvu') {
+                        return <Image
+                            source={Knovvu32}
+                            resizeMode="stretch"
+                            style={{
+                                width: 50,
+                                height: 30,
+                            }} />
+                    }
+                    return <Ionicons name={iconName || ''} size={26} color={focused ? color_200 : color_300} />;
+                }
+
                 return (
                     <TouchableOpacity
                         accessibilityRole="button"
@@ -47,9 +92,10 @@ function TabBarComponent({ state, descriptors, navigation }) {
                         testID={options.tabBarTestID}
                         onPress={onPress}
                         onLongPress={onLongPress}
-                        style={{ flex: 1 }}
+                        style={{ flexDirection: 'column', alignItems: 'center', flexGrow: 1, width: dynamicWidth, padding: 6 }}
                     >
-                        <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+                        {getIconFunc(isFocused)}
+                        <Text style={{ color: isFocused ? color_200 : color_300, fontSize: 11 }}>
                             {label}
                         </Text>
                     </TouchableOpacity>
@@ -60,3 +106,15 @@ function TabBarComponent({ state, descriptors, navigation }) {
 }
 
 export default TabBarComponent;
+
+{/*<TouchableOpacity
+                        accessibilityRole="button"
+                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        style={{ flex: 1 }}
+                    >
+                    
+                </TouchableOpacity>*/}
--- src/routes/navigator.tsx
+++ src/routes/navigator.tsx
@@ -25,7 +25,6 @@ const Navigator = () => {
     const color_100 = useAppSelector(state => state.theme.color_100);
     const color_200 = useAppSelector(state => state.theme.color_200);
     const color_300 = useAppSelector(state => state.theme.color_300);
-    const modalVisible = useAppSelector(state => state.main.modalVisible);
 
     useEffect(() => {
         dispatch(asyncGetWebchatData());
@@ -34,38 +33,11 @@ const Navigator = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator
+                tabBar={props => <TabBarComponent {...props} />}
                 screenOptions={({ route }) => ({
                     title: route.name === "Knovvu" ? "" : route.name,
                     headerRight: () => <Image source={Knovvu32} style={{ width: 32, height: 32, marginHorizontal: 16 }} />,
                     headerTintColor: color_100,
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-
-                        if (route.name === 'Home') {
-                            iconName = focused
-                                ? 'ios-home'
-                                : 'ios-home-outline';
-                        } else if (route.name === 'Settings') {
-                            iconName = focused ? 'ios-build' : 'ios-build-outline';
-                        }
-                        else if (route.name === 'About') {
-                            iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
-                        }
-                        else if (route.name === 'Contact Us') {
-                            iconName = focused ? 'ios-mail-sharp' : 'ios-mail-outline';
-                        }
-                        else if (route.name === 'Knovvu') {
-                            return <Image
-                                source={Knovvu32}
-                                resizeMode="contain"
-                                style={{
-                                    marginTop: 20,
-                                    height: Dimensions.get('screen').height * 0.07,
-                                    width: Dimensions.get('screen').width * 0.2
-                                }} />
-                        }
-                        return <Ionicons name={iconName || ''} size={size} color={color} />;
-                    },
                     tabBarActiveTintColor: color_200,
                     tabBarInactiveTintColor: color_300,
                 })}
@@ -76,14 +48,7 @@ const Navigator = () => {
                 <Drawer.Screen name="Settings" component={Settings} />
                 <Drawer.Screen
                     name="Knovvu"
-                    component={KnovvuCmp}
-                    listeners={({ navigation, route }) => ({
-                        state: (e) => {
-                            console.log(e.data?.state)
-                            if (modalVisible) modalVisible();
-                            navigation.goBack();
-                        },
-                    })} />
+                    component={KnovvuCmp} />
             </Tab.Navigator>
 
         </NavigationContainer>
