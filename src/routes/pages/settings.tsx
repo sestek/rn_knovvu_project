2a36d0316f2653df109e61f514e696a02e0ddf4c
Rabia <rabiaa_vural@hotmail.com>
2023-09-13 09:21:49 +0300

    fetch token added

--- src/routes/pages/settings.tsx
+++ src/routes/pages/settings.tsx
@@ -96,6 +96,74 @@ const Settings = () => {
   }, [webchat]);
 
   useEffect(() => {
+     const apiUrl = 'https://api-gateway.sestek.com/wtIdentity'
+    const requestData = {
+      client_id: 'Wagtail_Test',
+      client_secret: '1q2w3E*',
+      grant_type: 'client_credentials',
+    };
+    const encode2 = (str) =>
+    encodeURIComponent(str)
+        .replace(/\!/g, '%21')
+        .replace(/\~/g, '%7E')
+        .replace(/\*/g, '%2A')
+        .replace(/\'/g, '%27')
+        .replace(/\(/g, '%28')
+        .replace(/\)/g, '%29');
+    const toFormUrlEncoded = data =>
+      Object.entries(data)
+        .map(
+          ([key, value]) =>
+            `${encode2(key)}=${encode2(value)}`,
+        )
+        .join('&');
+
+    const formData = toFormUrlEncoded(requestData);
+    console.log(formData)
+
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('API yanıtı:', data);
+      })
+      .catch(error => {
+        console.error('API isteği sırasında hata oluştu:', error);
+      });
+
+    
+  }, []);
+
+  useEffect(() => {
+    const socket = new WebSocket('wss://nesibe-yilmaz-tokyo.wagtail.test.core.devops.sestek.com.tr/project-runner/chatgpt');
+
+    socket.onopen = () => {
+      console.log('WebSocket bağlantısı başarıyla açıldı.');
+    };
+
+    socket.onmessage = (e) => {
+      console.log('WebSocket mesajı alındı:', e.data);
+    };
+
+    socket.onclose = (e) => {
+      console.log('WebSocket bağlantısı kapatıldı:', e.reason);
+    };
+
+    socket.onerror = (e) => {
+      console.error('WebSocket hatası:', e.message);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  useEffect(() => {
     axios
       .get('https://api-gateway.sestek.com/get-demos')
       .then(response => {
