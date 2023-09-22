044f0747d737c84acf92692f162f70f4c99d43f1
TAYFUN-KAYA <tayfunkaya.uni@gmail.com>
2023-09-22 10:48:00 +0300

    solved socket send

--- src/routes/pages/chatGpt.tsx
+++ src/routes/pages/chatGpt.tsx
@@ -22,9 +22,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Lottie from 'lottie-react-native';
 import MessageBoxBody from '@src/components/chatgpt/MessageBoxBody';
 import useModalCloseStore from '../../zustandStore/store';
+import {decode, encode} from 'base-64';
+// import {decode, encode} from 'base64-js';
+import {Buffer} from 'buffer';
+
+const recorder = new AudioRecorderPlayer();
 
 const ChatGpt = ({navigation}) => {
-  const recorder = new AudioRecorderPlayer();
   const [recordStatus, setRecordStatus] = useState(false);
   const [pancMessage, setPancMessage] = useState(
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
@@ -39,11 +43,16 @@ const ChatGpt = ({navigation}) => {
   //   {
   //     headers: {
   //       ['Authorization']: `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjIwNDhENDI5NjI0QzAzRDQ3NzIyNEQyOEQ5REFDMEY3IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2OTQxNzE2MTEsImV4cCI6MTcyNTcwNzYxMSwiaXNzIjoiL2NvcmUtYmFja2VuZC1hdXRoIiwiYXVkIjoiV2FndGFpbCIsImNsaWVudF9pZCI6IldhZ3RhaWxfVGVzdCIsImlhdCI6MTY5NDE3MTYxMSwic2NvcGUiOlsiV2FndGFpbCJdfQ.rxQj4xbm7r9tFsFD-RlVxyOZKRViERryuo9biGwsLRLCdTZhoOEJuqhoS-dHoWxGsm1Tw5rwV0p9mLnDBewxm7vWsolK9N-F2y1xSxpyDG6azLuR-rGD2wlfkRTvR-T5exI3OQ4G8AQM86k3Wrw5pk2XLKc8zh-1ju1TFtblTLTX68sDChKrSZWbHvxcnilrR0Y_gINtRlSjfX1dSibMcQ-hIctQqIVHokwbTj1EfYsdsVBMn61XCXnNPN_QyNvW2o01yxILeCKgQYyetTXImUlnk-KsC_e33vU2eAXa5xjPoZ_5xjtnLE1_m0yFmZPN_2-NOdI8qXif1XTan5SHGA`,
+  //       ['tenant']: '3a0cc777-85df-d0ec-0ef2-d0117048aab5',
   //     },
   //   },
   // );
 
-  // console.log(socket);
+  const socket = new WebSocket(
+    'wss://nesibe-yilmaz-tokyo.wagtail.test.core.devops.sestek.com.tr/project-runner/chatgpt',
+  );
+
+  //const socket = new WebSocket('wss://ws.bitstamp.net');
 
   const permission = async () => {
     if (Platform.OS === 'android') {
@@ -89,9 +98,13 @@ const ChatGpt = ({navigation}) => {
         .catch(err => console.log(err));
 
       const dirs = RNFetchBlob.fs.dirs.CacheDir + '/sestek_bot';
+      // const path = Platform.select({
+      //   ios: 'sestek_bot/' + createUUID() + '.m4a',
+      //   android: `${dirs}/${createUUID()}.wav`,
+      // });
       const path = Platform.select({
-        ios: 'sestek_bot/' + createUUID() + '.m4a',
-        android: `${dirs}/${createUUID()}.wav`,
+        ios: 'sestek_bot/' + 'sound' + '.m4a',
+        android: `${dirs}/sound.mp3`,
       });
       console.log(dirs);
       console.log(path);
@@ -109,82 +122,161 @@ const ChatGpt = ({navigation}) => {
     // }, 500);
   };
 
+  const atob = (input: string) => {
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+    let str = input.replace(/=+$/, '');
+    let output = '';
+
+    if (str.length % 4 == 1) {
+      throw new Error(
+        "'atob' failed: The string to be decoded is not correctly encoded.",
+      );
+    }
+    for (
+      let bc = 0, bs = 0, buffer, i = 0;
+      (buffer = str.charAt(i++));
+      ~buffer && ((bs = bc % 4 ? bs * 64 + buffer : buffer), bc++ % 4)
+        ? (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6))))
+        : 0
+    ) {
+      buffer = chars.indexOf(buffer);
+    }
+
+    return output;
+  };
+
+  // const Uint8ArrayFromBase64 = async (base64: any) => {
+  //   return Uint8Array.from(atob(base64), v => v.charCodeAt(0));
+  // };
+
+  const Uint8ArrayFromBase64 = async (value: any) => {
+    const binaryString = decode(value);
+    //console.log('rif:', binaryString);
+    const uint8Array = new Uint8Array(binaryString.length);
+
+    for (let i = 0; i < binaryString.length; i++) {
+      uint8Array[i] = binaryString.charCodeAt(i);
+    }
+
+    return uint8Array;
+  };
+
   const handleSend = async () => {
-    console.log('ses gönderiliyor');
-    const result = await recorder.stopRecorder();
-    recorder.removeRecordBackListener();
-    console.log('removed back listener');
-    setRecordStatus(false);
     try {
+      await recorder.stopRecorder();
+      recorder.removeRecordBackListener();
+      setRecordStatus(false);
+      // ----------------------
       // const dirFile = result.split('/');
       // const data =
       //   RNFetchBlob.fs.dirs.CacheDir +
-      //   '/sestek_bot' +
+      //   '/sestek_bot/' +
       //   dirFile[dirFile.length - 1];
+      // console.log('eski data : ', data);
       // const SendData = {
       //   url: result,
       //   data: await RNFetchBlob.fs.readFile(data, 'base64'),
       // };
-      // //socket.send(SendData.data);
-      // fetch('https://e1f7-176-88-223-122.ngrok.io/base64-aac-to-wav', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'text/plain',
-      //   },
-      //   body: SendData.data,
-      // })
-      //   .then(response => response.json())
-      //   .then(data => {
-      //     console.log('API yanıtı:', data.access_token);
-      //   })
-      //   .catch(error => {
-      //     console.error('API isteği sırasında hata oluştu:', error);
-      //   });
-      // console.log(SendData.data);
+      // console.log(SendData);
+      // ----------------------
+      // socket.send(
+      //   JSON.stringify({
+      //     event: 'bts:subscribe',
+      //     data: {channel: 'order_book_btcusd'},
+      //   }),
+      // );
+      // ----------------------
+
+      const soundPath = Platform.select({
+        ios: RNFetchBlob.fs.dirs.CacheDir + '/sestek_bot/sound.m4a',
+        android: RNFetchBlob.fs.dirs.CacheDir + `/sound.mp3`,
+      });
+
+      if (soundPath) {
+        console.log('sound path : ', soundPath);
+        const audioToBase64 = await RNFetchBlob.fs.readFile(
+          soundPath,
+          'base64',
+        );
+        let myBase64 = '';
+        fetch('https://api-gateway.sestek.com/base64-aac-to-wav', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+          body: audioToBase64,
+        })
+          .then(response => response.text())
+          .then(data => {
+            myBase64 = data;
+          })
+          .catch(error => {
+            console.error('API isteği sırasında hata oluştu:', error);
+          });
+        
+        const convertedAudio = await Uint8ArrayFromBase64(myBase64);
+        //console.log(convertedAudio.toString());
+        console.log('gönderiliyor !');
+        socket.send(
+          JSON.stringify({
+            'message-name': 'start',
+            audio: {
+              'sample-rate': '8000',
+              'channel-count': '1',
+            },
+            ca: {
+              user_properties: {},
+            },
+            settings: {
+              channel_tags: null,
+            },
+            tenant_id: '3a0cc777-85df-d0ec-0ef2-d0117048aab5',
+          }),
+        );
+        socket.send(convertedAudio);
+      }
+
+      // ----------------------
     } catch (error) {
       console.log(error);
     }
     //return {url: result, data: await RNFetchBlob.fs.readFile(data, 'base64')};
   };
 
-  // useEffect(() => {
-  //   try {
-  //     socket.onopen = (event: any) => {
-  //       //socket.send(JSON.stringify(apiCall));
-  //       console.log('Web Socket open', event);
-  //       socket.send(
-  //         JSON.stringify({
-  //           'message-name': 'start',
-  //           audio: {
-  //             'sample-rate': '8000',
-  //             'channel-count': '1',
-  //           },
-  //           ca: {
-  //             user_properties: {},
-  //           },
-  //           settings: {
-  //             channel_tags: null,
-  //           },
-  //           tenant_id: '3a0cc777-85df-d0ec-0ef2-d0117048aab5',
-  //         }),
-  //       );
-  //     };
-  //     socket.onmessage = e => {
-  //       console.log('WebSocket mesajı alındı:', e.data);
-  //     };
-  //     socket.onclose = e => {
-  //       console.log('WebSocket bağlantısı kapatıldı:', e.reason);
-  //     };
-  //     socket.onerror = e => {
-  //       console.error('WebSocket hatası:', e.message);
-  //     };
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  //   return () => {
-  //     socket.close();
-  //   };
-  // }, []);
+  socket.onmessage = e => {
+    console.log('Mesaj:', e.data);
+  };
+  socket.onclose = e => {
+    console.log('WebSocket bağlantısı kapatıldı:', e.reason);
+  };
+  socket.onerror = e => {
+    console.error('WebSocket hatası:', e.message);
+  };
+
+  useEffect(() => {
+    socket.onopen = (event: any) => {
+      //socket.send(JSON.stringify(apiCall));
+      console.log('Web Socket open', event);
+      socket.send(
+        JSON.stringify({
+          'message-name': 'start',
+          audio: {
+            'sample-rate': '8000',
+            'channel-count': '1',
+          },
+          ca: {
+            user_properties: {},
+          },
+          settings: {
+            channel_tags: null,
+          },
+          tenant_id: '3a0cc777-85df-d0ec-0ef2-d0117048aab5',
+        }),
+      );
+    };
+  }, []);
 
   const [fakeMessages, setFakeMessages] = useState([
     {
